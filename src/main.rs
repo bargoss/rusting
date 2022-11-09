5bc1928953f1fdc72b17f5f05adeee150c1456b7
bargoss <yesiltepebaran98@gmail.com>
2022-11-09 23:36:59 +0300

    just the mongodb examples

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
-#![feature(proc_macro_hygiene, decl_macro)]
 
-
-#[macro_use] extern crate rocket;
-
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
-}
-
-fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
-}
+/*
+connect in javascript:
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://asdasdajklj232:<password>@cluster0.c54afg2.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+*/
 
 //username:
 //asdasdajklj232
@@ -19,28 +18,45 @@ fn main() {
 //asdasd97gf8asdad
 
 
-//use mongodb::{Client, options::{ClientOptions, ResolverConfig}};
-//use std::env;
-//use std::error::Error;
-//use tokio;
-//#[tokio::main]
-//async fn mongo_example() -> Result<(), Box<dyn Error>> {
-//    // Load the MongoDB connection string from an environment variable:
-//    let client_uri =
-//        env::var("MONGODB_URI").expect("You must set the MONGODB_URI environment var!");
-//
-//    // A Client is needed to connect to MongoDB:
-//    // An extra line of code to work around a DNS issue on Windows:
-//    let options =
-//        ClientOptions::parse_with_resolver_config(&client_uri, ResolverConfig::cloudflare())
-//            .await?;
-//    let client = Client::with_options(options)?;
-//
-//    // Print the databases in our MongoDB cluster:
-//    println!("Databases:");
-//    for name in client.list_database_names(None, None).await? {
-//        println!("- {}", name);
-//    }
-//
-//    Ok(())
-//}
+use mongodb::{Client, Collection, options::{ClientOptions, ResolverConfig}};
+use std::env;
+use std::error::Error;
+use mongodb::bson::Document;
+use tokio;
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    // Load the MongoDB connection string from an environment variable:
+    //let client_uri =
+    //    env::var("MONGODB_URI").expect("You must set the MONGODB_URI environment var!");
+    let client_uri = "mongodb+srv://asdasdajklj232:asdasd97gf8asdad@cluster0.c54afg2.mongodb.net/?retryWrites=true&w=majority";
+
+    // A Client is needed to connect to MongoDB:
+    // An extra line of code to work around a DNS issue on Windows:
+    let options =
+        ClientOptions::parse_with_resolver_config(&client_uri, ResolverConfig::cloudflare())
+            .await?;
+    let client = Client::with_options(options)?;
+
+
+    // Print the databases in our MongoDB cluster:
+    println!("Databases:");
+    for name in client.list_database_names(None, None).await? {
+        println!("- {}", name);
+    }
+
+
+    let coll: Collection<Document> = client.database("test").collection("devices");
+
+
+
+    // print records
+    let cursor = coll.find(None, None).await?;
+    for result in cursor {
+        if let Ok(item) = result {
+            println!("Found document: {:?}", item);
+        }
+    }
+
+
+    Ok(())
+}
